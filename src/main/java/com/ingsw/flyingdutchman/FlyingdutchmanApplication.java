b7547b725172e6e8b8cc411c9453b91f13233b52
FedericoChiodi <federico.chiodi@edu.unife.it>
2024-07-25 14:59:29 +0200

    First commit

---
+++ src/main/java/com/ingsw/flyingdutchman/FlyingdutchmanApplication.java
@@ -0,0 +1,13 @@
+package com.ingsw.flyingdutchman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyingdutchmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyingdutchmanApplication.class, args);
+	}
+
+}
