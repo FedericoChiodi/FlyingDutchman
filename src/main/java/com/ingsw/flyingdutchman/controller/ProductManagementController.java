495bb56510d91237efd53d28cf13e9c1184a8161
FedericoChiodi <federico.chiodi@edu.unife.it>
2024-08-06 12:05:15 +0200

    Added Product controller

---
+++ src/main/java/com/ingsw/flyingdutchman/controller/ProductManagementController.java
@@ -0,0 +1,129 @@
+package com.ingsw.flyingdutchman.controller;
+
+
+import com.ingsw.flyingdutchman.model.mo.*;
+import com.ingsw.flyingdutchman.model.service.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+
+@Controller
+@RequestMapping("/productManagement")
+public class ProductManagementController {
+
+    private final ProductService productService;
+    private final UserService userService;
+    private final AuctionService auctionService;
+    private final CategoryService categoryService;
+    private final OrderService orderService;
+
+    @Autowired
+    private ProductManagementController(ProductService productService, UserService userService, AuctionService auctionService, CategoryService categoryService, OrderService orderService){
+        this.productService = productService;
+        this.userService = userService;
+        this.auctionService = auctionService;
+        this.categoryService = categoryService;
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/view")
+    public String view(HttpServletRequest request){
+        User loggedUser = userService.findLoggedUser(request);
+
+        return prepareProducts(request, loggedUser);
+    }
+
+    @GetMapping("/insert")
+    public String insertView(HttpServletRequest request){
+        User loggedUser = userService.findLoggedUser(request);
+        List<Category> categories = categoryService.getAllCategoriesExceptPremium();
+
+        request.setAttribute("loggedOn", loggedUser != null);
+        request.setAttribute("loggedUser", loggedUser);
+        request.setAttribute("categories", categories);
+
+        return "productManagement/insertView";
+    }
+
+    @PostMapping("/insert")
+    public String insert(HttpServletRequest request){
+        User loggedUser = userService.findLoggedUser(request);
+
+        String imagePath = "/home/sanpc/tomcat/webapps/Uploads/" + loggedUser.getUsername() + File.separator + request.getParameter("description") + ".png";
+        productService.createProduct(
+                request.getParameter("description"),
+                Float.parseFloat(request.getParameter("min_price")),
+                Float.parseFloat(request.getParameter("starting_price")),
+                Float.parseFloat(request.getParameter("current_price")),
+                imagePath,
+                categoryService.findCategoryById(Long.valueOf(request.getParameter("categoryID"))),
+                loggedUser
+        );
+
+        List<Product> products = productService.findProductByOwnerNotDeletedNotSold(loggedUser);
+
+        request.setAttribute("loggedOn", true);
+        request.setAttribute("loggedUser", loggedUser);
+        request.setAttribute("products", products);
+        request.setAttribute("soldProductsAction", false);
+
+        return "productManagement/view";
+    }
+
+    @DeleteMapping("/delete")
+    public String delete(HttpServletRequest request){
+        User loggedUser = userService.findLoggedUser(request);
+        Product product = productService.findProductById(Long.valueOf(request.getParameter("productID")));
+        List<Auction> auctions = auctionService.findByProductOwnerOpenNotDeleted(product);
+
+        if(!auctions.isEmpty()){
+            request.setAttribute("applicationMessage", "Non puoi eliminare un prodotto attualmente in asta!");
+        }
+        else{
+            productService.deleteProduct(product);
+        }
+
+        return prepareProducts(request, loggedUser);
+    }
+
+    @RequestMapping("/viewSold")
+    public String viewSoldProducts(HttpServletRequest request){
+        User loggedUser = userService.findLoggedUser(request);
+        List<Product> products = productService.findProductByOwnerNotDeletedNotSold(loggedUser);
+        List<User> buyers = new ArrayList<>();
+
+        for(Product product : products){
+            Order order = orderService.findOrderByProduct(product);
+            buyers.add(userService.findByUserID(order.getBuyer().getUserID()));
+        }
+
+        request.setAttribute("loggedOn", loggedUser != null);
+        request.setAttribute("loggedUser", loggedUser);
+        request.setAttribute("products", products);
+        request.setAttribute("buyers", buyers);
+        request.setAttribute("soldProductsAction", true);
+
+        return "productManagement/view";
+    }
+
+    @NotNull
+    private String prepareProducts(HttpServletRequest request, User loggedUser) {
+        List<Product> products = productService.findProductByOwnerNotDeletedNotSold(loggedUser);
+
+        request.setAttribute("loggedOn", loggedUser != null);
+        request.setAttribute("loggedUser", loggedUser);
+        request.setAttribute("products", products);
+        request.setAttribute("soldProductsAction", false);
+
+        return "productManagement/view";
+    }
+}
