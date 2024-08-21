373f0c797d025f0ea8fa225c378b78291323bbc0
FedericoChiodi <federico.chiodi@edu.unife.it>
2024-08-21 16:40:01 +0200

    test workflow

--- src/main/java/com/ingsw/flyingdutchman/FlyingdutchmanApplication.java
+++ src/main/java/com/ingsw/flyingdutchman/FlyingdutchmanApplication.java
@@ -1,9 +1,7 @@
 package com.ingsw.flyingdutchman;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -11,11 +9,6 @@ public class FlyingdutchmanApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlyingdutchmanApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(@NotNull SpringApplicationBuilder application) {
-		return application.sources(FlyingdutchmanApplication.class);
 	}
 
 }
