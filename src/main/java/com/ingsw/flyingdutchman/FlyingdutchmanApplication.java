b2a97b9e270cb22d7e705f1248f14c44cb641bb8
FedericoChiodi <federico.chiodi@edu.unife.it>
2024-08-21 22:42:37 +0200

    Restored annotation

--- src/main/java/com/ingsw/flyingdutchman/FlyingdutchmanApplication.java
+++ src/main/java/com/ingsw/flyingdutchman/FlyingdutchmanApplication.java
@@ -1,7 +1,9 @@
 package com.ingsw.flyingdutchman;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class FlyingdutchmanApplication {
 
 	public static void main(String[] args) {
