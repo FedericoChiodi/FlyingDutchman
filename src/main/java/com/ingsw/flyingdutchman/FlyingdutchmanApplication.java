a3d1f9edd9db224e1178c49a2114a431f4db4f33
FedericoChiodi <federico.chiodi@edu.unife.it>
2024-08-21 14:17:12 +0200

    Added war packaging

--- src/main/java/com/ingsw/flyingdutchman/FlyingdutchmanApplication.java
+++ src/main/java/com/ingsw/flyingdutchman/FlyingdutchmanApplication.java
@@ -1,13 +1,21 @@
 package com.ingsw.flyingdutchman;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlyingdutchmanApplication {
+public class FlyingdutchmanApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlyingdutchmanApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(@NotNull SpringApplicationBuilder application) {
+		return application.sources(FlyingdutchmanApplication.class);
 	}
 
 }
