049e3c3ffe1411c9a2bc58acdfca527d3397fd8f
FedericoChiodi <federico.chiodi@edu.unife.it>
2024-08-21 22:03:41 +0200

    Removed filter

--- src/main/java/com/ingsw/flyingdutchman/FlyingdutchmanApplication.java
+++ src/main/java/com/ingsw/flyingdutchman/FlyingdutchmanApplication.java
@@ -2,9 +2,10 @@ package com.ingsw.flyingdutchman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class FlyingdutchmanApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
