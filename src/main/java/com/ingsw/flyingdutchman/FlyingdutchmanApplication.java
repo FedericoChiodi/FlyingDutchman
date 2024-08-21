e10087fdf2fecb00854061753821bd2b62c0adfb
FedericoChiodi <federico.chiodi@edu.unife.it>
2024-08-21 22:28:20 +0200

    Updated dependency version

--- src/main/java/com/ingsw/flyingdutchman/FlyingdutchmanApplication.java
+++ src/main/java/com/ingsw/flyingdutchman/FlyingdutchmanApplication.java
@@ -1,12 +1,8 @@
 package com.ingsw.flyingdutchman;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
-public class FlyingdutchmanApplication extends SpringBootServletInitializer {
+public class FlyingdutchmanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlyingdutchmanApplication.class, args);
