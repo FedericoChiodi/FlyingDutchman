5811588cdc48e92d3ac0a24f30d76083cf36d5fb
FedericoChiodi <federico.chiodi@edu.unife.it>
2024-08-22 14:57:58 +0200

    Removed unused methods

--- src/main/java/com/ingsw/flyingdutchman/model/service/OrderService.java
+++ src/main/java/com/ingsw/flyingdutchman/model/service/OrderService.java
@@ -31,22 +31,6 @@ public class OrderService {
         orderRepository.save(order);
     }
 
-    public void deleteOrder(Order order) {
-        orderRepository.delete(order);
-    }
-
-    public Order updateOrder(Order order) {
-        return orderRepository.save(order);
-    }
-
-    public Order findOrderById(Long orderId) {
-        return orderRepository.findById(orderId).orElse(null);
-    }
-
-    public Order findOrderByProduct(Product product) {
-        return orderRepository.findByProduct(product);
-    }
-
     public List<Order> findOrdersByUser(User user) {
         return orderRepository.findByBuyer(user);
     }
