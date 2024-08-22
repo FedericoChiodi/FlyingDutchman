8b15b1bc1e4c735fac273b5117716923114e2714
FedericoChiodi <federico.chiodi@edu.unife.it>
2024-08-22 16:14:37 +0200

    Added context

--- src/test/java/com/ingsw/flyingdutchman/FlyingdutchmanApplicationTests.java
+++ src/test/java/com/ingsw/flyingdutchman/FlyingdutchmanApplicationTests.java
@@ -2,8 +2,10 @@ package com.ingsw.flyingdutchman;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FlyingdutchmanApplicationTests {
 
 	@Test
