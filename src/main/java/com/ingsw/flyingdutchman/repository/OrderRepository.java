28bcddfce3ebfcd592a259674eea5e8b63c38c23
FedericoChiodi <federico.chiodi@edu.unife.it>
2024-08-22 14:58:49 +0200

    Removed unused methods

--- src/main/java/com/ingsw/flyingdutchman/repository/OrderRepository.java
+++ src/main/java/com/ingsw/flyingdutchman/repository/OrderRepository.java
@@ -1,7 +1,6 @@
 package com.ingsw.flyingdutchman.repository;
 
 import com.ingsw.flyingdutchman.model.mo.Order;
-import com.ingsw.flyingdutchman.model.mo.Product;
 import com.ingsw.flyingdutchman.model.mo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,9 +10,7 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    // Trova Ordine dal Prodotto comprato
-    Order findByProduct(Product product);
-
     // Trova Ordini dall'Utente che li ha piazzati
     List<Order> findByBuyer(User buyer);
+
 }
